a28456602e97b2583234a475b285a0dc45c647c6
amanynmohamed <amanynmohamed@gmail.com>
2025-04-17 23:12:54 +0200

    Add files via upload
     Python OOP Challenge project

---
+++ Digital pet project.py
@@ -0,0 +1,13 @@
+from pet import Pet
+
+my_pet = Pet("Fluffy")
+
+my_pet.get_status()
+my_pet.eat()
+my_pet.play()
+my_pet.sleep()
+my_pet.get_status()
+
+my_pet.train("roll over")
+my_pet.train("play dead")
+my_pet.show_tricks()
